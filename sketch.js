71e29c24aa277faab642888a68bf58e544cdfb99
Arnav0285 <76435154+Arnav0285@users.noreply.github.com>
2021-01-18 20:11:42 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,40 +1,75 @@
-var cat, catImage
-var mouse, mouseImage
-var background, backgroundImage
+var ground, groundImg;
+
+var jerry, jerry_run;
+
+var tom, tom_running;
 
 function preload() {
     //load the images here
-    catImage = loadAnimation("tomOne.png,tomTwo.png,tomThree.png,tomFour.png");
-    mouseImage = loadAnimation("jerryOne.png,jerryTwo.png,jerryThree.png,jerryFour.png");
-    backgroundImage = loadImage("garden.png");
+
+    groundImg = loadImage("garden.png");
+
+    jerryImg = loadAnimation("jerryOne.png");
+
+    jerryImage4 = loadAnimation("jerryFour.png");
+
+    jerry_run = loadAnimation("jerryTwo.png", "jerryThree.png");
+
+    tomImg1 = loadAnimation("tomOne.png");
+
+    tomImg4 = loadAnimation("tomFour.png");
+
+    tom_running = loadAnimation("tomTwo.png", "tomThree.png");
+
 }
 
 function setup(){
-    createCanvas(1000,800);
-    //create tom and jerry sprites here
-    cat = createSprite(800,700);
-    cat.addAnimation("catImage");
+    createCanvas(680,400);
+    
+    ground = createSprite(340, 200);
+    ground.addImage("groundImg", groundImg);
+    ground.scale = 0.7;
 
-    mouse = createSprite(200,700);
-    mouse.addAnimation("mouseImage");
+    tom = createSprite(500, 340, 20, 100);
+    tom.addAnimation("tomImg1", tomImg1);
+    tom.scale = 0.1;
 
-    background = createSprite(500,400,1000,800);
-    background.addImage("backgroundImage");
+    jerry = createSprite(200, 340, 10, 40);
+    jerry.addAnimation("jerryImg", jerryImg);
+    jerry.scale = 0.1;
 
 }
 
 function draw() {
 
-    
-    //Write condition here to evalute if tom and jerry collide
+    console.log(tom.x - jerry.x);
+
+    background(rgb(30, 100, 220));
+
+if (tom.x - jerry.x < tom.width / 2 - jerry.width / 2) {
+       tom.velocityX = 0;
+       tom.addAnimation("tomImg4", tomImg4);
+       tom.changeAnimation("tomImg4");
+       tom.x = 300;
+
+       jerry.addAnimation("jerryImage4", jerryImage4);
+       jerry.changeAnimation("jerryImage4");
+       tom.bounce(jerry);
+    }
 
     drawSprites();
 }
 
+function keyPressed() {
+    
+    if (keyCode === LEFT_ARROW) {
 
-function keyPressed(){
+        tom.velocityX = -5;
+        tom.addAnimation("tom_running", tom_running);
+        tom.changeAnimation("tom_running");
 
-  //For moving and changing animation write code here
-
-
+        jerry.addAnimation("jerry_run", jerry_run);
+        jerry.changeAnimation("jerry_run");
+    }
+    
 }
